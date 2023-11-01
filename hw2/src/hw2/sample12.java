0290721eb61f6b2c900c6f7e2a9b388d46a3241f
HongMingE <t110360150@ntut.org.tw>
2023-11-01 23:12:16 +0800

    hw2 1102

---
+++ hw2/src/hw2/sample12.java
@@ -0,0 +1,15 @@
+package hw2;
+
+public class sample12
+{
+	public static void main(String[] args)
+	{
+		int i = 1;
+		do {
+			System.out.println("第" +i+ "次的迴圈");
+			i++;
+		}while(i <= 5);
+		
+		System.out.println("迴圈結束");
+	}
+}
